15c805b4d2e5ec580dc204e6e3d480f4f10411d3
Grexy <Grexy@users.noreply.github.com>
2015-11-03 12:24:43 -0500

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,8 @@
+# name: spoiler-btn
+# about: spoiler button for discourse 
+# version: 1.0
+# authors: Steven Greco
+
+### Spoiler post toolbar button
+register_asset "javascripts/spoiler-toolbar.js", :composer
+register_asset "stylesheets/spoiler-toolbar.css.scss"
